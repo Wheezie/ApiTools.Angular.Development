fb430e201f06e73567dc88ffc6b2b5da45cc69df
Wraptor <wraptor@wheezie.be>
2020-02-01 13:18:17 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'apitools-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ApiTools-Angular';
+}
